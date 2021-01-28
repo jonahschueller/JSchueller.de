f494646619c996a98c77cf24239965cd64f512a9
Bohne9 <jonah@die-schuellers.de>
2021-01-28 22:01:19 +0100

    ✨ Home page.

--- pages/index.js
+++ pages/index.js
@@ -5,60 +5,31 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+        <div className={styles.meContainer}>
+          <div className={styles.meColumn}>
+            <h1>Jonah Schüller</h1>
+            <h4 className={styles.subtitle}>SOFTWARE DEVELOPER</h4>
+            <p className={styles.meText}>
+              Hey there, my name is Jonah Schüller. Currently, I am studying
+              computer science at RWTH Aachen University. 🤓 I love to explore the world of ☁️ cloud computing.
             </p>
-          </a>
+          </div>
+          <div className="meColumn right">
+            <img src="/me.png" className={styles.avatar}/>
+          </div>
+
         </div>
+        
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        
       </footer>
     </div>
   )
