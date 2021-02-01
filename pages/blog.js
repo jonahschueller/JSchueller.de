308ece0c4bd2a9b9c8e4ca35c169cb76faa68e00
Bohne9 <jonah@die-schuellers.de>
2021-02-01 22:04:46 +0100

    ✨ Post fetch lib

--- pages/blog.js
+++ pages/blog.js
@@ -2,12 +2,16 @@ import Link from 'next/link'
 import styles from '../styles/Blog.module.css';
 import commonStyles from '../styles/Content.module.css';
 import Page from '../common/Page'
+import { loadCategories } from '../lib/post';
 
-const Blog = () => {
+
+const Blog = (props) => {
+    console.log(props);
+
     return (
     <Page title="Blog">
         <div>
-            <Link href="/blog">
+            <Link href="/blog#blog">
                 <div className={commonStyles.title}>
                     &#x2192; Blog
                 </div>
@@ -18,8 +22,23 @@ const Blog = () => {
                     &#x2192; Featured
                 </div>
             </Link>
+            {/* {
+                props.categories.map(item => {
+                    return (<div>{item}</div>)
+                })
+            } */}
         </div>
     </Page>)
+}
+
+export const getStaticProps = async () => {
+    let cate = loadCategories()
+
+    return {
+        props: {
+            categories: cate
+        }
+    }
 }
 
 export default Blog;
